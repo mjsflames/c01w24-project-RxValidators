1684d3ce83f0c36b657ae2eec65ce09e0011cc10
renathossain <renathossain@gmail.com>
2024-02-28 12:49:43 -0500

    Added cpsm spider and remade the tester

---
+++ on_bc/cpsm_spider.py
@@ -0,0 +1,52 @@
+from scrapy import Spider, Request, FormRequest
+from scrapyscript import Job, Processor
+from bs4 import BeautifulSoup
+import json
+
+processor = Processor(settings={"LOG_ENABLED": False})
+
+class CPSMSpider(Spider):
+    name = 'cpsm_spider'
+
+    def __init__(self, last_name, first_name, *args, **kwargs):
+        super(CPSMSpider, self).__init__(*args, **kwargs)
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def start_requests(self):
+        url = f"https://member.cpsm.mb.ca/api/physicianprofile/searchresult?lastname={self.last_name}&firstname={self.first_name}"
+        yield Request(url, callback=self.parse)
+
+    def parse(self, response):
+        data = json.loads(response.text)
+        if not data["items"]:
+            yield {"status": "NOT FOUND"}
+        else:
+            id_no = data["items"][0]["links"][0]["parameters"]
+            url = f"https://member.cpsm.mb.ca/api/physicianprofile/practitionerinformation?id={id_no}"
+            yield Request(url, callback=self.after_submit)
+
+    def after_submit(self, response):
+        data = json.loads(response.text)
+        status = data['membershipClass'].lower()
+        if "regulated member" in status:
+            yield {"status": "VERIFIED"}
+        else:
+            yield {"status": "INACTIVE"}
+
+def cpsm_spider(last_name, first_name):
+    job = Job(CPSMSpider, last_name, first_name)
+    return processor.run(job)[0]["status"]
+
+if __name__ == "__main__":
+    print("Fake person test case:")
+    print("Last Name: \"Willie\", First Name: \"Wonka\"")
+    print(cpsm_spider("Willie", "Wonka"))
+
+    print("Real person (active) test case:")
+    print("Last Name: \"Roy\", First Name: \"Danielle\"")
+    print(cpsm_spider("Roy", "Danielle"))
+
+    print("Real person (inactive) test case:")
+    print("Last Name: \"Adediji\", First Name: \"Uchechukwu Okwudili\"")
+    print(cpsm_spider("Adediji", "Uchechukwu Okwudili"))
--- on_bc/test_verification.py
+++ on_bc/test_verification.py
@@ -1,30 +1,35 @@
+import os
 import pandas as pd
 from tqdm import tqdm
 from cpso_spider import cpso_spider
-from cpsbc_spider import cpsbc_spider 
+from cpsbc_spider import cpsbc_spider
+from cpsm_spider import cpsm_spider
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
 CPSBC_str = "College of Physicians and Surgeons of British Columbia"
 CPSO_str = "College of Physicians and Surgeons of Ontario"
+CPSM_str = "College of Physicians and Surgeons of Manitoba"
 
-def create_dataframe():
+def main():
     df = pd.read_csv("sample_data.csv")
     df_CPSBC = df["Licensing College"] == CPSBC_str
     df_CPSO = df["Licensing College"] == CPSO_str
-    df = df[df_CPSBC | df_CPSO]
+    df_CPSM = df["Licensing College"] == CPSM_str
+    df = df[df_CPSBC | df_CPSO | df_CPSM]
     df['Scraped Status'] = None
 
-    # # Iterate over each row and update the "Scraped Status" column accordingly
+    # Iterate over each row and update the "Scraped Status" column accordingly
     for index, row in tqdm(df.iterrows(), total=len(df), desc="Scraping in Progress"):
         if row['Licensing College'] == CPSBC_str:
             df.at[index, 'Scraped Status'] = cpsbc_spider(row['Last Name'], row['First Name'])
         elif row['Licensing College'] == CPSO_str:
             df.at[index, 'Scraped Status'] = cpso_spider(row['Last Name'], row['First Name'], row['Licence #'])
-    
-    return df
+        elif row['Licensing College'] == CPSM_str:
+            df.at[index, 'Scraped Status'] = cpsm_spider(row['Last Name'], row['First Name'])
+        os.system('cls' if os.name == 'nt' else 'clear')
+        tqdm.write(str(df))
 
 if __name__ == "__main__":
-    df = create_dataframe()
-    print(df)
+    main()
