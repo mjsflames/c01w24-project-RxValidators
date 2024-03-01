a5228edd58081f562bc37f5edf5a3df892773911
Danny <77023480+TheDanDan@users.noreply.github.com>
2024-02-29 21:01:34 -0500

    Created the webscrapers for Quebec and  Nova Scotia

---
+++ tutorial/spiders/quebec_spider.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "Quebec"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_info = kwargs.get('user_info')
+
+    def start_requests(self):
+        status = "NOT FOUND"
+        name = input("Enter name: ")
+        number = input("Enter number: ")
+
+        # Create a new instance of the Chrome driver
+        driver = webdriver.Chrome()
+
+        driver.get(f"https://www.cmq.org/fr/bottin/medecins?number={number}&lastname={name}&firstname=&specialty=0&city=&unlisted=false")
+
+        try:
+            cookiesElement = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="didomi-notice-disagree-button"]'))
+            )
+
+            cookiesElement.click()
+
+            try:
+              element = WebDriverWait(driver, 2).until(
+                  EC.element_to_be_clickable((By.XPATH, '//*[@id="__nuxt"]/div/div/main/article/section[2]/div/div/table/tbody/tr[1]'))
+              )
+              element.click()
+            except TimeoutException:
+              status = "NOT FOUND"
+              return status
+
+            element.click()
+
+            popupElement = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, '/html/body/div[3]/div/div/div/div[2]/ul/li[4]'))
+            )
+            popup_text = popupElement.text
+        finally:
+            return status
---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "NovaScotia"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_info = kwargs.get('user_info')
+
+    def start_requests(self):
+        status = "NOT FOUND"
+        fname = input("Enter fname: ")
+        lname = input("Enter lname: ")
+        number = input("Enter number: ")
+        driver = webdriver.Chrome()
+
+        driver.get("https://cpsnsphysiciansearch.azurewebsites.net/")
+
+        try:
+            fName_field = driver.find_element(By.ID, "firstname")
+            fName_field.send_keys(fname)
+
+            lName_field = driver.find_element(By.ID, "lastname")
+            lName_field.send_keys(lname)
+
+            lNum_field = driver.find_element(By.ID, "licencenumber")
+            lNum_field.send_keys(number)
+
+            submit = driver.find_element(By.ID, "search")
+            submit.click()
+            time.sleep(30)
+
+            try:
+              element = WebDriverWait(driver, 2).until(
+                  EC.visibility_of_element_located((By.XPATH, '//*[@id="grid-basic"]/tbody/tr[1]/td[1]/a'))
+              )
+              element.click()
+            except TimeoutException:
+              status = "NOT FOUND"
+
+            licenseStatus = WebDriverWait(driver, 2).until(
+                EC.visibility_of_element_located((By.ID, 'MainContent_frmlicencetype'))
+            )
+
+            status = licenseStatus.get_property("value")
+
+        finally:
+            return status
