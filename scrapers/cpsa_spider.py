2451f8b4937de645596ba9e48c69d0e0e8540f3f
renathossain <renathossain@gmail.com>
2024-03-04 10:57:37 -0500

    Was able to derive the cookie in alberta spider, but it still does not work. Realized that is javascript enabled website that requires the use of scrapy-selenium

--- scrapers/cpsa_spider.py
+++ scrapers/cpsa_spider.py
@@ -3,32 +3,34 @@ import webbrowser
 
 class CPSASpider(Spider):
     name = "cpsa_spider"
+    start_urls = ["https://search.cpsa.ca/"]
 
     def __init__(self, last_name, first_name, *args, **kwargs):
         super(CPSASpider, self).__init__(*args, **kwargs)
         self.last_name = last_name
         self.first_name = first_name
+        self.cookies = {}
 
-    def start_requests(self):
-        url = "https://search.cpsa.ca/"
-        yield Request(
-            url, callback=self.parse
-        )
+    def parse_cookies(self, response):
+        for cookie in response.headers.getlist('Set-Cookie'):
+            cookie_name, cookie_value = cookie.decode('utf-8').split(';')[0].split('=')
+            self.cookies[cookie_name] = cookie_value
+        print(self.cookies)
 
     def parse(self, response):
-        cookies = {
-            "ASP.NET_SessionId": "tu41v1bckwlhxoib0ysp4pa3"
-        }
+        callback = None
+        if self.cookies == {}:
+            callback = self.parse
+            self.parse_cookies(response)
+        else:
+            callback = self.after_submit
         formdata = {
             "ctl00$ctl16": "ctl00$ctl16|ctl00$MainContent$physicianSearchView$btnSearch",
-            "ctl00$MainContent$physicianSearchView$txtFirstName": "Anne-Josee",
-            "ctl00$MainContent$physicianSearchView$txtLastName": "Cote",
+            "ctl00$MainContent$physicianSearchView$txtFirstName": self.first_name,
+            "ctl00$MainContent$physicianSearchView$txtLastName": self.last_name
         }
-        yield FormRequest(
-            url=response.url,
-            formdata=formdata,
-            cookies=cookies,
-            callback=self.after_submit
+        yield FormRequest.from_response(
+            response, cookies=self.cookies, formdata=formdata, callback=callback
         )
 
     def after_submit(self, response):
