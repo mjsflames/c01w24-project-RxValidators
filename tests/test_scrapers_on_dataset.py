15ffe841c788c468346de10e160bf9d200de0a04
renathossain <renathossain@gmail.com>
2024-03-04 19:26:46 -0500

    simplified tester

--- tests/test_scrapers_on_dataset.py
+++ tests/test_scrapers_on_dataset.py
@@ -8,7 +8,6 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
 df = pd.read_csv("tests/physician_data.csv")
-passed_count, failed_count = 0, 0
 failures = []
 
 @pytest.fixture(scope="session", autouse=True)
@@ -16,22 +15,19 @@ def print_message_after_tests(request):
     yield
     print("")
     print("")
-    global passed_count, failed_count, failures
-    print(f"Passed - {passed_count}, Failed - {failed_count}")
+    global failures
     for failure in failures:
         print(failure)
 
 @pytest.mark.parametrize("i", range(len(df)))
 def test_assertions(i, capfd):
-    global df, passed_count, failed_count, failures
+    global df, failures
     row = df.iloc[i]
     status = verify(row['Last Name'], row['First Name'], \
                     row['Licence #'], row['Province'])
     if status == row['Status']:
-        passed_count += 1
         assert True
     else:
-        failed_count += 1
         failures.append((row['Last Name'], row['First Name'], \
                          row['Licence #'], row['Province']))
         assert False
