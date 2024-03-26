135ba8a8a98ca4beeab6437675dfd36a6c643ac0
Mitch <mitchzhu55@gmail.com>
2024-03-25 23:59:06 -0400

    edited frontend pages and corrected links

--- frontend/src/pages/PatientPrescriptions.jsx
+++ frontend/src/pages/PatientPrescriptions.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import PageHeader from "../components/PageHeader";
 import Prescription from "../components/Prescription";
 import ContentContainer from "../components/ContentContainer";
+import pic from "../assets/prescribertable.jpg";
 
 const PatientPrescriptions = () => {
   const [data, setData] = useState(null);
@@ -75,46 +76,50 @@ const PatientPrescriptions = () => {
         title="My Prescriptions"
         desc="Check the statuses of your prescriptions and find out if you are eligible for a Discovery Pass."
       />
-      <ContentContainer>
-      <div className="flex flex-col mx-auto mb-12 text-center">
-      <h1 className="text-3xl underline font-bold !text-gray-900  mb-5">First/Last Name's Logged Prescriptions</h1>
-          <p className="font-semibold">Click on Show More to access more details about your prescription. Prescription status will automatically change once both parties log the prescription.</p>
-          <p className="font-semibold">If you are prescribed a Discovery Pass, it will be mailed to the address on file.</p>
-      </div>
-        <table id="patientPrescriptionTable" className="w-full mt-10 mb-20 text-sm rtl:text-right text-gray-500">
-          <thead className="text-xs text-left text-black uppercase bg-PaRxGreen">
-            <tr>
-              <th className="text-left p-2 w-1/8">Date</th>
-              <th className="text-left w-1/8">Prescriber Code</th>
-              <th className="text-left w-1/8">Status</th>
-              <th className="text-left w-1/8">Discovery Pass</th>
-              <th className="w-1/2 text-left px-2">Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data && data.map((item) => (
-              <>
-                <tr className="text-left border-t border-white odd:bg-[#8fbc8f]/30 even:bg-white">
-                  <td className="p-2 w-1/8">{item.date}</td>
-                  <td className="w-1/8">{item.prescriber_code}</td>
-                  <td className="w-1/8">{item.status}</td>
-                  <td className="w-1/8"><input type="checkbox" checked={item.discovery} disabled /></td>
-                  <td className="px-2 w-1/2 truncate max-w-md">{item.comments}
-                    <button onClick={() => itemClick(item)} className="float-right">
-                      <p className="font-bold">Show More</p>
-                    </button>
-                  </td>
-                </tr>{myItem === item && (<tr className="text-left border-t border-white bg-[#f5fffa]">
-                  <td colSpan="5">
-                    <Prescription item={item} />
-                  </td>
+      <div className="flex w-full h-[650px] items-center justify-center bg-cover" style={{backgroundImage: `url(${pic})`}}>
+        <ContentContainer>
+        <div class="rounded-xl w-screen bg-gray-200 bg-opacity-70 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+          <div className="flex flex-col mx-auto mb-12 text-center">
+          <h1 className="text-3xl underline font-bold !text-gray-900  mb-5">First/Last Name's Logged Prescriptions</h1>
+              <p className="font-semibold">Click on Show More to access more details about your prescription. Prescription status will automatically change once both parties log the prescription.</p>
+              <p className="font-semibold">If you are prescribed a Discovery Pass, it will be mailed to the address on file.</p>
+          </div>
+            <table id="patientPrescriptionTable" className="w-full mt-10 mb-20 text-sm rtl:text-right text-gray-500">
+              <thead className="text-xs text-left text-black uppercase bg-[#f0fff0]">
+                <tr>
+                  <th className="text-left p-2 w-1/8">Date</th>
+                  <th className="text-left w-1/8">Prescriber Code</th>
+                  <th className="text-left w-1/8">Status</th>
+                  <th className="text-left w-1/8">Discovery Pass</th>
+                  <th className="w-1/2 text-left px-2">Details</th>
                 </tr>
-                )}
-              </>
-            ))}
-          </tbody>
-        </table>
-      </ContentContainer>
+              </thead>
+              <tbody>
+                {data && data.map((item) => (
+                  <>
+                    <tr className="text-left text-black border-t border-white odd:bg-white/60 even:text-white even:bg-[#0a0e1a]/50">
+                      <td className="p-2 w-1/8">{item.date}</td>
+                      <td className="w-1/8">{item.prescriber_code}</td>
+                      <td className="w-1/8">{item.status}</td>
+                      <td className="w-1/8"><input type="checkbox" checked={item.discovery} disabled /></td>
+                      <td className="px-2 w-1/2 truncate max-w-md">{item.comments}
+                        <button onClick={() => itemClick(item)} className="float-right">
+                          <p className="font-bold">Show More</p>
+                        </button>
+                      </td>
+                    </tr>{myItem === item && (<tr className="text-left border-t border-white">
+                      <td colSpan="5">
+                        <Prescription item={item} />
+                      </td>
+                    </tr>
+                    )}
+                  </>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </ContentContainer>
+      </div>
     </>
   );
 };
