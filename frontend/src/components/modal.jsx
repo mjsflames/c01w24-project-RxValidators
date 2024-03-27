8a65673c683209c300b5c9a449fba074afd93547
Mitch <mitchzhu55@gmail.com>
2024-03-27 16:42:18 -0400

    edited modal to delete user on button click

--- frontend/src/components/modal.jsx
+++ frontend/src/components/modal.jsx
@@ -1,8 +1,49 @@
-import React, { useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { UserContext } from '../App';
+import api from "../axiosConfig";
+import { useNavigate } from "react-router-dom";
 
 const Modal = () => {
     const [showModal, setShowModal] = useState(false);
+    const [data, setData] = useState("");
+    const { getEmail } = useContext(UserContext);
     
+    useEffect(() => {
+        if (getEmail) setData(getEmail);
+
+        const deleteHandler = async () => {
+            try {
+                const res = await api.delete(`http://localhost:5001/auth/removeUser/${data.email}`)
+                console.log(res.data)
+                useNavigate("/login")
+            } catch (error) {
+                setError(err.res.data.message);
+                console.log(err.res)
+            }
+        }
+        deleteHandler();
+    }, []);
+      
+    
+    // useEffect(() => {
+    //     async function removeUser() {
+    //         const username = user.email;
+    //         try {
+    //             const res = await fetch(`http://localhost:5001/auth/removeUser/${username}`, {
+    //                 method: "DELETE",
+    //             })
+    //             .then(res => res.json())
+    //             .then(res => console.log(res))
+    //             if(res.status !== 200) {
+    //                 console.log("User deletion failed");
+    //             }
+    //         } catch (error) {
+    //             console.error('Failed to fetch username', error)
+    //         }
+    //     }
+    //     removeUser();
+    // }, []);
+
     return (
         <>
             <button id="deactivate" onClick={() => setShowModal(true)} className="bg-red-600 text-white hover:bg-red-200 text-sm rounded-full p-2.5">
@@ -22,7 +63,7 @@ const Modal = () => {
                                     <p>Disclaimer: If your account is deleted, your prescriptions may remain.</p>
                                 </div>
                                 <div className="flex flex-row justify-center mb-5">
-                                    <button id="deleteaccount" className="bg-green-200 hover:bg-green-200/40 text-black border rounded-full p-2.5 mr-10" onClick={() => setShowModal(false)}>Yes, proceed</button>
+                                    <button id="deleteaccount" onClick={useEffect} className="bg-green-200 hover:bg-green-200/40 text-black border rounded-full p-2.5 mr-10">Yes, proceed</button>
                                     <button className="bg-red-200 hover:bg-red-200/40 text-black border rounded-full p-2.5" onClick={() => setShowModal(false)}>No, cancel</button>
                                 </div>
                             </div>  
