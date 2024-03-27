7e34ced12c4268ec2b30cdd4538e1bca5e8b2f35
Mitch <mitchzhu55@gmail.com>
2024-03-26 21:38:40 -0400

    made small edits

--- frontend/src/components/modal.jsx
+++ frontend/src/components/modal.jsx
@@ -6,7 +6,7 @@ const Modal = () => {
     return (
         <>
             <button id="deactivate" onClick={() => setShowModal(true)} className="bg-red-600 text-white hover:bg-red-200 text-sm rounded-full p-2.5">
-                DEACTIVATE ACCOUNT    
+                DELETE ACCOUNT    
             </button>
             {showModal ? (
                 <>
@@ -14,18 +14,18 @@ const Modal = () => {
                         <div className="relative w-auto mx-auto">
                             <div className="border-0 rounded-lg relative flex flex-col w-full bg-white outline-none px-10"> 
                                 <div className="flex items-start justify-between p-5">
-                                    <h2 className="text-l font-bold mx-auto">Please Confirm Deactivation</h2>
+                                    <h2 className="text-l font-bold mx-auto">Please Confirm Deletion</h2>
                                     <button className="bg-transparent text-black absolute right-2 top-0" onClick={() => setShowModal(false)}>X</button>
                                 </div>
-                                <div className="relative flex-auto mb-5">
-                                    <p>Are you sure you want to send a deactivation request?</p>
+                                <div className="relative text-center flex-auto mb-5">
+                                    <p>Are you sure you want to delete your account?</p>
+                                    <p>Disclaimer: If your account is deleted, your prescriptions may remain.</p>
                                 </div>
                                 <div className="flex flex-row justify-center mb-5">
                                     <button id="deleteaccount" className="bg-green-200 hover:bg-green-200/40 text-black border rounded-full p-2.5 mr-10" onClick={() => setShowModal(false)}>Yes, proceed</button>
                                     <button className="bg-red-200 hover:bg-red-200/40 text-black border rounded-full p-2.5" onClick={() => setShowModal(false)}>No, cancel</button>
                                 </div>
-                            </div>
-                            
+                            </div>  
                         </div>
                     </div>
                 </>
