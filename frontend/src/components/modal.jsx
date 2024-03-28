39e72221a5a1a8131752d2ab03630297711433d9
Mitch <mitchzhu55@gmail.com>
2024-03-27 21:42:14 -0400

    added functionality for deletion buttons in modal

--- frontend/src/components/modal.jsx
+++ frontend/src/components/modal.jsx
@@ -5,44 +5,26 @@ import { useNavigate } from "react-router-dom";
 
 const Modal = () => {
     const [showModal, setShowModal] = useState(false);
-    const [data, setData] = useState("");
-    const { getEmail } = useContext(UserContext);
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
     
-    useEffect(() => {
-        if (getEmail) setData(getEmail);
-
-        const deleteHandler = async () => {
-            try {
-                const res = await api.delete(`http://localhost:5001/auth/removeUser/${data.email}`)
-                console.log(res.data)
-                useNavigate("/login")
-            } catch (error) {
-                setError(err.res.data.message);
-                console.log(err.res)
+    const deleteHandler = async () => {
+        if (!user) return;
+        try {
+            let username = user.email;
+            if (user.role === "prescriber") {
+                username = user["providerCode"]
             }
+
+            const res = await api.delete(`/auth/removeUser/${username}`)
+            console.log(res.data)
+            navigate("/login")
+        } catch (err) {
+            setError(err.response);
+            console.log(err.res)
         }
-        deleteHandler();
-    }, []);
-      
-    
-    // useEffect(() => {
-    //     async function removeUser() {
-    //         const username = user.email;
-    //         try {
-    //             const res = await fetch(`http://localhost:5001/auth/removeUser/${username}`, {
-    //                 method: "DELETE",
-    //             })
-    //             .then(res => res.json())
-    //             .then(res => console.log(res))
-    //             if(res.status !== 200) {
-    //                 console.log("User deletion failed");
-    //             }
-    //         } catch (error) {
-    //             console.error('Failed to fetch username', error)
-    //         }
-    //     }
-    //     removeUser();
-    // }, []);
+    }
 
     return (
         <>
@@ -63,7 +45,7 @@ const Modal = () => {
                                     <p>Disclaimer: If your account is deleted, your prescriptions may remain.</p>
                                 </div>
                                 <div className="flex flex-row justify-center mb-5">
-                                    <button id="deleteaccount" onClick={useEffect} className="bg-green-200 hover:bg-green-200/40 text-black border rounded-full p-2.5 mr-10">Yes, proceed</button>
+                                    <button id="deleteaccount" onClick={deleteHandler} className="bg-green-200 hover:bg-green-200/40 text-black border rounded-full p-2.5 mr-10">Yes, proceed</button>
                                     <button className="bg-red-200 hover:bg-red-200/40 text-black border rounded-full p-2.5" onClick={() => setShowModal(false)}>No, cancel</button>
                                 </div>
                             </div>  
