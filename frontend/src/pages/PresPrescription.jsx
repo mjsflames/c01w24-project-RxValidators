680df67df7f0ec3e4ddf647bb2ede5e3a13353db
Mitch <mitchzhu55@gmail.com>
2024-03-29 14:34:20 -0400

    added in functionality for deleting a prescription log

--- frontend/src/pages/PresPrescription.jsx
+++ frontend/src/pages/PresPrescription.jsx
@@ -10,35 +10,36 @@ const PrescriberPrescriptions = () => {
   const [myItem, setItem] = useState(null);
   const { user } = useContext(UserContext);
   const [userData, setUserData] = useState();
+  const [error, setError] = useState("");
 
-  useEffect(() => {
-    const sampleData = [
-      {
-        "date": "2024-03-12",
-        "patient_initials": "AB",
-        "prescriber_code": "001",
-        "status": "Pending",
-        "comments": "Details for item 1",
-        "discovery": true,
-      },
-      {
-        "date": "2024-03-12",
-        "patient_initials": "CD",
-        "prescriber_code": "001",
-        "status": "Completed",
-        "comments": "Details for item 2 asjdfklajsdlfj alsdfjaslkdfjas lasdjfaslkdjf alsdkjfalskdjfaslkdfjalksdjf aslkdfjalsd fal"
-      },
-      {
-        "date": "2024-03-12",
-        "patient_initials": "EF",
-        "prescriber_code": "001",
-        "status": "Pending",
-        "comments": "Details for item 3"
-      }
-    ];
+  // useEffect(() => {
+  //   const sampleData = [
+  //     {
+  //       "date": "2024-03-12",
+  //       "patient_initials": "AB",
+  //       "prescriber_code": "001",
+  //       "status": "Pending",
+  //       "comments": "Details for item 1",
+  //       "discovery": true,
+  //     },
+  //     {
+  //       "date": "2024-03-12",
+  //       "patient_initials": "CD",
+  //       "prescriber_code": "001",
+  //       "status": "Completed",
+  //       "comments": "Details for item 2 asjdfklajsdlfj alsdfjaslkdfjas lasdjfaslkdjf alsdkjfalskdjfaslkdfjalksdjf aslkdfjalsd fal"
+  //     },
+  //     {
+  //       "date": "2024-03-12",
+  //       "patient_initials": "EF",
+  //       "prescriber_code": "001",
+  //       "status": "Pending",
+  //       "comments": "Details for item 3"
+  //     }
+  //   ];
 
-    setData(sampleData)
-  }, []);
+  //   setData(sampleData)
+  // }, []);
 
   useEffect(() => {
     async function fetchData() {
@@ -59,7 +60,6 @@ const PrescriberPrescriptions = () => {
       } catch (error) {
         console.error('No fetch:', error);
       }
-
     }
     fetchData();
   }, []);
@@ -77,13 +77,28 @@ const PrescriberPrescriptions = () => {
     }
   };
 
+  const deleteHandler = async (id) => {
+    console.log(id)
+    try {
+        const res = await fetch(`http://localhost:5001/api/delete/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+    } catch (err) {
+        setError(err.response);
+        console.log(err.res)
+    }
+}
+
   return (
     <>
       <PageHeader
         title="My Prescriptions"
         desc="Check all of your prescriptions all in one place."
       />
-      <div className="flex w-full h-[650px] items-center justify-center bg-cover" style={{backgroundImage: `url(${pic})`}}>
+      <div className="flex w-full items-center justify-center bg-cover" style={{backgroundImage: `url(${pic})`}}>
 
         <div class="rounded-xl w-3/4 bg-gray-200 bg-opacity-70 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
         <div className="flex flex-col mx-auto mb-12 text-center">
@@ -102,7 +117,7 @@ const PrescriberPrescriptions = () => {
                 <th scope="col" className="px-2 py-3 text-nowrap">Discovery Pass?</th>
                 <th scope="col" className="px-2 py-3">Prescriber Comments</th>
                 <th scope="col" className="px-2 py-3"></th>
-
+                <th scope="col" className="px-2 py-3"></th>
               </tr>
             </thead>
             <tbody>
@@ -114,6 +129,9 @@ const PrescriberPrescriptions = () => {
                     <td className="px-2 py-3">{item.status}</td>
                     <td className="px-2 py-3 w-1/8 pointer-events-none"><input type="checkbox" checked={item.discovery}/></td>
                     <td className="px-2 py-3 w-1/2 truncate max-w-md">{item.comments}</td>
+                    <td>
+                      <button class="btn btn-danger" onClick={() => deleteHandler(item._id)}>Delete</button>
+                    </td>
                     <button onClick={() => itemClick(item)} className="p-2 w-1/8">
                       <p className="font-bold text-nowrap underline">Show More</p>
                     </button>
@@ -128,7 +146,6 @@ const PrescriberPrescriptions = () => {
             </tbody>
           </table>
           </div>
-
       </div>
     </>
   );
