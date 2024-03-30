4f434fffc496f64d5a50304fee82b80f6a6147f5
JoeZhou1234 <87938075+JoeZhou1234@users.noreply.github.com>
2024-03-30 11:20:02 -0400

    Removed unnecessary imports

--- backend/auth_service/tests.py
+++ backend/auth_service/tests.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from database_Authentications import app, client, db, collection
+from database_Authentications import app
 
 class TestDatabaseAuthenticationService(unittest.TestCase):
     def setUp(self):
