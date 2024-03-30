df37cdada855284f3079e36d0d8d59ecef4ed2c0
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-03-29 22:34:13 -0400

    Fixing formatting of Admin Home page

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -14,43 +14,32 @@ const AdminHome = () => {
       <div className="w-full h-[650px] bg-white flex items-center pl-16">
         <div className="flex flex-col items-center gap-12 z-20">
           <div className="flex flex-col font-bold">
-            <h1>
-              See all accounts in one place. <br /> Verify providers,
-            </h1>
+            <h1>See all accounts in one place. <br /> Verify providers,</h1>
             <h2 className="text-green-700">in one click.</h2>
           </div>
-          <div className="flex flex-col gap-2  w-full">
-            <div className="flex flex-col lg:flex-row gap-2  w-full ">
+
+          <div className="flex flex-col gap-2 w-full">
+            <div className="flex flex-col lg:flex-row gap-2 w-full">
               <Link to="/verify">
-                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12  w-full lg:w-[12vw] text-center">
-                  Verify New Prescribers
-                </button>
+                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">Verify New Prescribers</button>
               </Link>
               <Link to="/green-resources">
-                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12  w-full lg:w-[12vw] text-center">
-                  Green Resources
-                </button>
+                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">Green Resources</button>
               </Link>
-            </div>
-            <div className="flex flex-col lg:flex-row gap-2  w-full ">
               <Link to="/adminPrescriberProfile">
-                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12  w-full lg:w-[12vw] text-center">
-                  Access Prescribers
-                </button>
+                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">Access Prescribers</button>
               </Link>
               <Link to="/adminPatientProfile">
-                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">
-                  Access Patients
-                </button>
+                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">Access Patients</button>
               </Link>
+              </div>
             </div>
           </div>
+          <div className="relative w-full h-full">
+            <img src={nature} className="h-full w-full object-cover absolute" />
+            <div className="w-full h-full bg-gradient-to-r from-white via-white/20 via-30% to-transparent absolute"></div>
+          </div>
         </div>
-        <div className="relative w-full h-full">
-          <img src={nature} className="h-full w-full object-cover absolute" />
-          <div className="w-full h-full bg-gradient-to-r from-white via-white/20 via-30% to-transparent absolute"></div>
-        </div>
-      </div>
     </>
   );
 };
