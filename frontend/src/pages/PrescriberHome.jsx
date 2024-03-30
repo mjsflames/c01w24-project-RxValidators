b8eb3ed3c354a0431348387a69177c2046c0b67d
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-03-30 00:31:23 -0400

    Fixing responsive text+buttons in PrescriberHome.jsx

--- frontend/src/pages/PrescriberHome.jsx
+++ frontend/src/pages/PrescriberHome.jsx
@@ -19,35 +19,35 @@ const PrescriberHome = () => {
 			desc="Welcome to your Parks Prescriptions account!"
 		  />
 		  <div className="flex w-full h-[650px] bg-white pl-16">
-			<div className="grid grid-cols-2 gap-y-0 w-screen-1/3 h-1/3 items-end">
-			  <div className="col-span-2 ml-5 mt-10 font-bold">
-				<h1>
-				  Connect to the evidence. <br /> Connect nature.
+			<div className="">
+			  <div className="col-span-2 mt-10 font-bold text-lg leading-9">
+				<h1 className="lg:text-5xl">
+				  Connect to the evidence.<br />Connect nature.
 				</h1>
 				<h2 className="text-green-700">Prescribe nature.</h2>
-				<p className="font-bold">
+				<p className="font-bold ">
 				  <br />
 				  Your PaRx Code: {userData?.providerCode}
 				</p>
 			  </div>
-			  <div className="col-span-2 mt-20 text-center z-20">
+			  <div className="col-span-2 mt-10 lg:mt-20 text-center items-center justify-center">
 				<h2 className="font-bold mb-2">Get Started</h2>
 				<Link to="/PrescriberLogRX">
-				  <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm ml-3 mr-8 h-12 w-2/3 text-center">
+				  <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">
 					Log a Patient Prescription
 				  </button>
 				</Link>
 	
-				<div className="flex gap-4 mt-4 justify-center items-center">
+				<div className="flex flex-col lg:flex-row gap-2 lg:gap-6 pt-2 lg:pt-6">
 				  <Link to="/PDF">
-					<button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-[12vw] text-center">
+					<button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">
 					  Download your Prescription PDF
 					</button>
 				  </Link>
 				  <Link to="/prescriberPrescriptions">
 					<button
 					  href="#"
-					  className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-[12vw] text-center"
+					  className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center"
 					>
 					  Access Your Prescriptions
 					</button>
@@ -55,7 +55,7 @@ const PrescriberHome = () => {
 				</div>
 			  </div>
 			</div>
-			<div className="relative w-full h-full bg-black">
+			<div className="relative w-full h-full">
 			  <img src={nature} className="h-full w-full object-cover absolute" />
 			  <div className="w-full h-full bg-gradient-to-r from-white via-white/20 via-30% to-transparent absolute"></div>
 			</div>
