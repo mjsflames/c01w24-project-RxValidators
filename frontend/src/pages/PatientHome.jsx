1a31a143d2966b16d888f93c9abd99a59610ee3b
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-03-29 21:41:39 -0400

    Made patient home page responsive

--- frontend/src/pages/PatientHome.jsx
+++ frontend/src/pages/PatientHome.jsx
@@ -10,37 +10,40 @@ const PrescriberHome = () => {
         title="Home"
         desc="Welcome to your Parks Prescriptions account!"
       />
-      <div className="flex w-full h-full bg-white">
-        <div className="grid grid-cols-2 gap-y-0 w-screen-1/3 h-1/3 items-end">
-          <div className="col-span-2 ml-5 mt-10 font-bold">
+      <div className="flex w-full h-[650px] bg-white pl-16">
+        <div className="gap-y-0 w-screen-1/3 h-1/3 items-end z-20">
+          <div className="col-span-2 mt-10 font-bold">
             <h1>
               Connect to the evidence. <br /> Your best health,
             </h1>
             <h2 className="text-green-700">naturally.</h2>
           </div>
-          <div className="col-span-2 mt-32 text-center mb-8">
+          <div className="col-span-2 mt-20 text-center">
             <h2 className="font-bold mb-2">Get Started</h2>
             <button
               onClick={() =>
                 window.open("https://www.parkprescriptions.ca/", "_blank")
               }
-              className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-64 text-center"
+              className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center"
             >
               Learn More About PaRx
             </button>
+
+            <div className="flex flex-col lg:flex-row gap-6 pt-6">
+              <Link to="/PatientLogRX">
+                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center">
+                  Log Prescriptions
+                </button>
+              </Link>
+              <Link to="/patientPrescriptions">
+                <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm h-12 w-full lg:w-[12vw] text-center align-middle">
+                  Access Your Prescriptions
+                </button>
+              </Link>
+            </div>
           </div>
-          <Link to="/PatientLogRX">
-            <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm ml-3 mr-16 h-12 w-64 text-center">
-              Log Prescriptions
-            </button>
-          </Link>
-          <Link to="/patientPrescriptions">
-            <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm mr-3 h-12 w-64 text-center align-middle">
-              Access Your Prescriptions
-            </button>
-          </Link>
         </div>
-        <div className="relative flex-1 w-full ">
+        <div className="relative w-full ">
           <img src={nature} className="h-full w-full object-cover absolute" />
           <div className="w-full h-full bg-gradient-to-r from-white via-white/20 via-30% to-transparent absolute"></div>
         </div>
